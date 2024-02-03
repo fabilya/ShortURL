e96fed04c38170febc479cd6070ae8a35968a520
Ilya Fabiyanskiy <ilya.fabiyanskiy@yandex.ru>
2024-02-03 18:45:29 +0300

    123

---
+++ yacut/utils.py
@@ -0,0 +1,16 @@
+import random
+from string import ascii_letters, digits
+
+SYMBOLS_CHOICE = list(ascii_letters + digits)
+
+
+def get_unique_short_url():
+    short = random.choices(SYMBOLS_CHOICE, k=6)
+    return ''.join(short)
+
+
+def check_symbols(custom_id):
+    for elem in custom_id:
+        if elem not in SYMBOLS_CHOICE:
+            return False
+    return True
